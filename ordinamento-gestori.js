c716ec6fff44c7bdb9c2871b6d710f53e9d8b4c5
Arslan <khurshid.arslan@gmail.com>
2020-08-19 14:40:29 +0200

    Stampa gestori

--- ordinamento-gestori.js
+++ ordinamento-gestori.js
@@ -5,9 +5,21 @@ console.log("Ciao");
 const fs = require("fs"); 
 fs.readFile("gestori_da_ordinare.json", function(err, data) {       
     if (err) throw err;  
-    const users = JSON.parse(data);      
-    console.log(users);  
+    const gestori = JSON.parse(data); 
+    var x=0;
+    gestori.forEach(element => {
+       
+        if(element.aree!=null){
+            console.log(element.aree.nome + " " + element.gestore.id.id + " " + element.gestore.nome);
+            
+        }
+        
+    });
+
+    
+      
 }); 
+
 
 
 
