65f811f09ea389ad1b1de15731b0325757638f26
Arslan <khurshid.arslan@gmail.com>
2020-08-19 17:01:42 +0200

    Code ready for angular

--- ordinamento-gestori.js
+++ ordinamento-gestori.js
@@ -7,16 +7,31 @@ fs.readFile("gestori_da_ordinare.json", function(err, data) {
     if (err) throw err;  
     const gestori = JSON.parse(data); 
     const sortedArray=gestori.sort(function(a,b){
-        return a.gestore.id.id - b.gestore.id.id;
+        if(a.aree!=null && b.aree!=null){
+            if(a.aree.id == b.aree.id){
+                return 1;
+            }else{
+                if(a.aree.nome>b.aree.nome){
+                    return +1;
+                }else{
+                    return -1;
+                }               
+            }
+        
+        }else{
+            return 1;
+        }
     });
 
     sortedArray.forEach(element => {
        
         if(element.aree!=null){
             
-            console.log(element.aree.nome + " " + element.gestore.id.id + " " + element.gestore.nome);           
+            console.log(element.aree.nome + " " + element.gestore.id.idUI + " " + element.gestore.id.id + " " + element.gestore.nome);           
         }      
     });
+
+    
 
 
     
