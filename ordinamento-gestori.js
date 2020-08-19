5d0b507653b3aebad7256edbda17ad9170011db6
Arslan <khurshid.arslan@gmail.com>
2020-08-19 15:14:16 +0200

    Sorted by ID

--- ordinamento-gestori.js
+++ ordinamento-gestori.js
@@ -6,15 +6,24 @@ const fs = require("fs");
 fs.readFile("gestori_da_ordinare.json", function(err, data) {       
     if (err) throw err;  
     const gestori = JSON.parse(data); 
-    var x=0;
-    gestori.forEach(element => {
+    const sortedArray=gestori.sort(function(a,b){
+        return a.gestore.id.id - b.gestore.id.id;
+    });
+
+    sortedArray.forEach(element => {
        
         if(element.aree!=null){
-            console.log(element.aree.nome + " " + element.gestore.id.id + " " + element.gestore.nome);
             
-        }
-        
+            console.log(element.aree.nome + " " + element.gestore.id.id + " " + element.gestore.nome);           
+        }      
     });
+
+
+    
+  
+
+
+
 
     
       
